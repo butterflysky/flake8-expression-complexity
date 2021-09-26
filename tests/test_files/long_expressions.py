c1f8c483dc295e9de049c1114038e4afec5c6a6e
Lina Edwards <lina@butterflysky.dev>
2021-09-26 14:52:19 -0700

    add extended test case for async for complexity

--- tests/test_files/long_expressions.py
+++ tests/test_files/long_expressions.py
@@ -53,7 +53,6 @@ sublist = weird_container[10:datetime.datetime.today(), None]
 with weird_container[10:str(datetime.datetime.today().date())[:100], None]:
     pass
 
+async for i in range(5 * len(str(len(range(2 * 5))) * 2[::-1][0:2] + "foo") / 3):
+    pass
 
-async def async_for_function():
-    async for i in range(10):
-        print(i)
